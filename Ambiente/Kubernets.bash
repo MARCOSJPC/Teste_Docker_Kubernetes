57d4abb4506d18f801ac9c7aac2c365a7a2637d0
Marcos Paulo <54601033+MARCOSJPC@users.noreply.github.com>
2022-06-29 12:04:09 -0300

    Iniciando a instalação do kubectl e minikube

--- Ambiente/Kubernets.bash
+++ Ambiente/Kubernets.bash
@@ -3,9 +3,12 @@ $ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/sta
 
 #dando a permissão de executavél dentro do sistema e movendo para o PATH
 $ chmod +x kubectl
-$ mv ./kubectl ~/.local/bin/kubectl
+$ sudo mv ./kubectl /usr/local/bin/kubectl
 $ kubectl version --client
 
 #Instalando MiniKube
 $ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 $ sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#Iniciando o cluster com virtualbox uma vez q o pŕoprio ja esteja instalado na maquina
+$ minikube start --vm-driver=virtualbox
