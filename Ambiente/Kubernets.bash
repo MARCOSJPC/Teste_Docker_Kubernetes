ae0897a7c5a4184e88cefdb30db7a823ebbc421e
Marcos Paulo <54601033+MARCOSJPC@users.noreply.github.com>
2022-06-29 18:41:14 -0300

    Update Kubernets.bash

--- Ambiente/Kubernets.bash
+++ Ambiente/Kubernets.bash
@@ -12,3 +12,17 @@ $ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 #Iniciando o cluster com virtualbox uma vez q o pŕoprio ja esteja instalado na maquina
 $ minikube start --vm-driver=virtualbox
+
+#definindo o valor da variavel de ambiente através de uma secret
+$ kubectl create secret generic var-ambiente-os \
+  --from-literal=TWORPTEST='true100%'
+
+#criando diretório e nosso arquivo yaml
+$ mkdir kubernetes-2rp
+$ cd kubernetes-2rp
+$ touch app-deployment.yaml ##arquivo encontrado na pasta anterior
+
+#executando nosso arquivo yaml
+$ kubectl apply -f ./app-deployment.yaml
+
+
