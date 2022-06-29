63b0a639c82b6594fc973a86cbfcf1f9556fff4c
Marcos Paulo <54601033+MARCOSJPC@users.noreply.github.com>
2022-06-29 16:48:49 -0300

    Update Docker.bash

--- Ambiente/Docker.bash
+++ Ambiente/Docker.bash
@@ -17,5 +17,58 @@
   
 #Adicionando o usuario ao grupo do docker:
  $ sudo usermod -aG docker $USER
+
+#Criando diretorio da aplicação, o arquivo da aplicação e o arquivo Dockerfile
+$ mkdir teste-variavel
+$ cd teste-variavel/
+$ touch index.js
+$ touch Dockerfile
+
+###Entrando no arquivo 'index.js' para edição
+$ vi index.js
+#adicionando as informações no editor de texto. Para salvar e sair só precionar "ESC", ":", "wq!" ou "x!"
+var Intervalo20 = setInterval(function(){
+    console.log(process.env.TWORPTEST);
+}, 20000);
+
+
+###Entrando e editando o arquivo Dockerfile
+$ vi Dockerfile
+#adicionando as informações no editor de texto.
+FROM node:14
+WORKDIR /app-node
+COPY . .
+RUN npm install
+ENTRYPOINT node index.js
+
+
+#Construindo a imagem
+$ docker build -t marcos/app-node:1.0 .
+#Após a criação deve-se upar a imagem no dockerhub mas é necessário ter uma conta no docker hub para isso
+#fazendo login no docker hub pelo terminal
+$ docker login -u dockerdoshock
+#mudando a tag da imagem criada para não ocorrer erro de permissão na hora de upar ela no dockerhub
+$ docker tag marcos/app-node:1.0 dockerdoshock/app-node:1.0
+#Upando a imagem no dockerhub
+docker push dockershock/app-node:1.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
